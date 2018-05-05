2458d099a41349fd02389d075cb3240b1007bec6
yasamoka <yasamoka@gmail.com>
2018-05-05 16:42:15 +0300

    implemented random spray generation on CPU

--- kernel.cu
+++ kernel.cu
@@ -4,6 +4,13 @@
 
 #include <stdio.h>
 #include <assert.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <random>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -31,8 +38,106 @@ __global__ void testKernel(uint8_t *inputImage, uint8_t *outputImage, unsigned i
 	}
 }
 
+void computeRandomSprays_CPU(short int ***spraysX, short int ***spraysY, const unsigned short int radius, const unsigned int numOfSamplePoints, const unsigned int numOfSprays) {
+	const unsigned int width = 2 * radius + 1;
+	const unsigned int area = width * width;								// compute area of neighborhood
+	bool *neighborhood = (bool*)malloc(area * sizeof(bool));				// allocate boolean neighborhood array of size area
+	short int *sprayX;														// short integer spray point abscissas array
+	short int *sprayY;														// short integer spray point ordinates array
+	unsigned int pointIdx;													// sample point index
+	float randomRadius;														// random radius
+	float randomTheta;														//random theta
+	short int randomPointX;													// random point abscissa
+	short int randomPointY;													// random point ordinate
+	unsigned int randomPointNeighborhoodIdx;								// random point neighborhood index
+	std::default_random_engine generator;									// random number generator engine
+	std::uniform_real_distribution<float> radiusDistribution(0, radius);	// uniform real distribution for radius in the range (0, radius)
+	std::uniform_real_distribution<float> thetaDistribution(0, 2 * M_PI);	// uniform real distribution for theta in the range (0, 2*pi)
+
+	*spraysX = (short int**)malloc(numOfSprays * sizeof(short int*));	// sprays abscissas array
+	*spraysY = (short int**)malloc(numOfSprays * sizeof(short int*));	// sprays ordinates array
+	
+	// initialize neighborbood as empty
+	for (unsigned int neighborIdx; neighborIdx < area; neighborIdx++) {
+		neighborhood[neighborIdx] = false;
+	}
+
+	const unsigned int centerPointNeighborhoodIdx = (width + 1) * radius; // calculate center point neighborhood index
+	neighborhood[centerPointNeighborhoodIdx] = true;	// block out upcoming random points from coinciding with the center point
+
+	// spray generation loop
+	for (unsigned int sprayIdx = 0; sprayIdx < numOfSprays; sprayIdx++) {
+		sprayX = (short int*)malloc(numOfSamplePoints * sizeof(short int));	// allocate spray point abscissas array of size numOfSamplePoints
+		sprayY = (short int*)malloc(numOfSamplePoints * sizeof(short int));	// allocate spray point abscissas array of size numOfSamplePoints
+		pointIdx = 0;											// reset sample point index to 0
+		while(pointIdx < numOfSamplePoints) {					// sample point loop
+			randomRadius = radiusDistribution(generator);	// get a random distance from the uniform real distribution for distance
+			randomTheta = thetaDistribution(generator);		// get a random theta from the uniform real distribution for theta
+			randomPointX = randomRadius * cos(randomTheta);	// compute random point abscissa
+			randomPointY = randomRadius * sin(randomTheta);	// compute random point ordinate
+			randomPointNeighborhoodIdx = width * (randomPointY + radius) + randomPointX + radius;	//compute random point neighborhood index
+
+			if (!neighborhood[randomPointNeighborhoodIdx]) {		// if the random point is not already a sample point
+				neighborhood[randomPointNeighborhoodIdx] = true;	// random point is now in the neighborhood
+				sprayX[pointIdx] = randomPointX;					// random point is now a sample point (abscissa)
+				sprayY[pointIdx] = randomPointY;					// random point is now a sample point (ordinate)
+				pointIdx++;											// advance point index
+			}
+		}
+		(*spraysX)[sprayIdx] = sprayX;				// add resultant spray abscissas to sprays abscissas
+		(*spraysY)[sprayIdx] = sprayY;				// add resultant spray ordinates to sprays ordinates
+		
+		// set neighborhood back to empty
+		for (pointIdx = 0; pointIdx < numOfSamplePoints; pointIdx++) {
+			randomPointNeighborhoodIdx = width * (sprayY[pointIdx] + radius) + sprayX[pointIdx] + radius;
+			neighborhood[randomPointNeighborhoodIdx] = false;	// remove each sample point from neighborhood
+		}
+	}
+
+	free(neighborhood);	// release allocated memory for neighborhood array
+}
+
+cv::Mat generateRandomSprayImage(short int *sprayX, short int *sprayY, const unsigned short int radius, const unsigned int numOfSamplePoints) {
+	const unsigned int width = radius * 2 + 1;
+	const unsigned int area = width * width;
+	uint8_t *neighborhood = (uint8_t*)malloc(area * sizeof(uint8_t));
+	for (unsigned int pointIdx = 0; pointIdx < area; pointIdx++) {
+		neighborhood[pointIdx] = 0;	// black image
+	}
+	
+	unsigned int pointNeighborhoodIdx;
+	for (unsigned int pointIdx = 0; pointIdx < numOfSamplePoints; pointIdx++) {
+		pointNeighborhoodIdx = width * (sprayY[pointIdx] + radius) + sprayX[pointIdx] + radius;
+		neighborhood[pointNeighborhoodIdx] = 255;	// white pixel where a sample point is present
+	}
+	
+	cv::Mat sprayImage(width, width, CV_8UC1, neighborhood);	//create OpenCV grayscale image from data
+	return sprayImage;
+}
+
 int main(int argc, char *argv[])
 {
+	srand(time(NULL));
+	const unsigned short int radius = 200;
+	const unsigned int numOfSamplePoints = 200;
+	const unsigned int numOfSprays = 100;
+	short int **spraysX;
+	short int **spraysY;
+	GpuTimer computeRandomSpraysCPUTimer;
+	computeRandomSpraysCPUTimer.Start();
+	computeRandomSprays_CPU(&spraysX, &spraysY, radius, numOfSamplePoints, numOfSprays);
+	computeRandomSpraysCPUTimer.Stop();
+
+	printf("Time to compute random sprays (CPU): %f ms\n", computeRandomSpraysCPUTimer.Elapsed());
+
+	printf("Writing random sprays (%i) to disk ...\n", numOfSprays);
+	char sprayImageName[20];
+	for (unsigned int sprayIdx = 0; sprayIdx < numOfSprays; sprayIdx++) {
+		cv::Mat sprayImage = generateRandomSprayImage(spraysX[sprayIdx], spraysY[sprayIdx], radius, numOfSamplePoints);
+		sprintf(sprayImageName, "spray%i.png", sprayIdx);
+		cv::imwrite(sprayImageName, sprayImage);
+	}
+
 	if (argc != 2) {
 		fprintf(stderr, "Invalid arguments.");
 		return 1;
@@ -61,12 +166,8 @@ int main(int argc, char *argv[])
     }
 
 	cv::Mat outputImage(inputImage.rows, inputImage.cols, CV_8UC3, outputImageData);
-	cv::namedWindow("Input Image", cv::WINDOW_NORMAL);
-	cv::namedWindow("Output Image", cv::WINDOW_NORMAL);
-	cv::imshow("Input Image", inputImage);
-	cv::imshow("Output Image", outputImage);
-
-	cv::waitKey(0);
+	printf("%s\n", "Writing output image to disk ...");
+	cv::imwrite("output.png", outputImage);
 
     return 0;
 }
